a7ce4ab272537bdaab7bb56a4e2010a8b77262da
Chris Sanders <NinjaZidane@hotmail.com>
2012-06-15 20:55:29 -0400

    [Bukkit] Added PermissionUtil and getSizeByPermFor method. Damn you Oracle >.> Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

---
+++ src/main/java/com/almuramc/backpack/bukkit/util/PermissionUtil.java
@@ -0,0 +1,44 @@
+package com.almuramc.backpack.bukkit.util;
+
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+
+/**
+ * Simple class to handle checking of permissions.
+ */
+public class PermissionUtil {
+	private static final String[] SIZE_PERMS = {
+		"backpack.size.9",
+		"backpack.size.18",
+		"backpack.size.27",
+		"backpack.size.36",
+		"backpack.size.45",
+		"backpack.size.54"
+	};
+
+	/**
+	 * Checks the permissions of a player and compares it to an array of size perms. This method
+	 * will return the size of a backpack or -1 if no size perm was found.
+	 * @param player
+	 * @return
+	 */
+	public static int getSizeByPermFor(Player player) {
+		String found;
+		int size = -1;
+		for (PermissionAttachmentInfo perm : player.getEffectivePermissions()) {
+			for (String sizePerm : SIZE_PERMS) {
+				//Find a valid size perm
+				if (perm.getPermission().equals(sizePerm)) {
+					//Store the found permission.
+					found = perm.getPermission();
+					//Splice out the size
+					int temp = Integer.parseInt(found.split("backpack.size.")[1]);
+					if (temp > size) {
+						size = temp;
+					}
+				}
+			}
+		}
+		return size;
+	}
+}
