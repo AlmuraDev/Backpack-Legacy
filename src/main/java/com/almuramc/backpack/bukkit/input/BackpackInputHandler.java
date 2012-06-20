07e1499a9f2babd66badd0b81244f4de3140634d
Chris Sanders <NinjaZidane@hotmail.com>
2012-06-20 03:51:19 -0400

    [Common] Drafted Almura Development License version 1. This license states that all classes that reference GPL code are inherently licensed as GPL (most of the classes in the bukkit package). Classes that do NOT reference GPL code are licensed under the LGPL. The second part may change based on the extent we are willing to let loose the code to. Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

--- src/main/java/com/almuramc/backpack/bukkit/input/BackpackInputHandler.java
+++ src/main/java/com/almuramc/backpack/bukkit/input/BackpackInputHandler.java
@@ -2,20 +2,27 @@
  * This file is part of Backpack.
  *
  * Copyright (c) 2012, AlmuraDev <http://www.almuramc.com/>
- * Backpack is licensed under the GNU Public License version 3.
+ * Backpack is licensed under the Almura Development License version 1.
  *
  * Backpack is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
+ * As an exception, all classes which reference GPL licensed code are hereby
+ * licensed under the GNU General Public License, as described in the
+ * Almura Development License version 1.
  *
  * Backpack is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Backpack.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * the GNU General Public License (for classes that fulfill the exception) and the
+ * Almura Development License version 1 along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU Lesser General Public License and
+ * the GNU General Public License.
  */
 package com.almuramc.backpack.bukkit.input;
 
