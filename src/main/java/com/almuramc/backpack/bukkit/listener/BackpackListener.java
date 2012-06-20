e3b90fb9ff6936e695299e7c6f6244dffe950f6d
Chris Sanders <NinjaZidane@hotmail.com>
2012-06-20 10:59:09 -0400

    [Common] Adjusted AlmuraDev license, I had it backwards. Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

--- src/main/java/com/almuramc/backpack/bukkit/listener/BackpackListener.java
+++ src/main/java/com/almuramc/backpack/bukkit/listener/BackpackListener.java
@@ -5,24 +5,24 @@
  * Backpack is licensed under the Almura Development License version 1.
  *
  * Backpack is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * As an exception, all classes which reference GPL licensed code are hereby
- * licensed under the GNU General Public License, as described in the
- * Almura Development License version 1.
+ * As an exception, all classes which do not reference GPL licensed code
+ * are hereby licensed under the GNU Lesser Public License, as described
+ * in themAlmura Development License version 1.
  *
  * Backpack is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License,
- * the GNU General Public License (for classes that fulfill the exception) and the
- * Almura Development License version 1 along with this program. If not, see
- * <http://www.gnu.org/licenses/> for the GNU Lesser General Public License and
- * the GNU General Public License.
+ * You should have received a copy of the GNU General Public License,
+ * the GNU Lesser Public License (for classes that fulfill the exception)
+ * and the Almura Development License version 1 along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU General Public License and
+ * the GNU Lesser Public License.
  */
 package com.almuramc.backpack.bukkit.listener;
 
