fffdd8dfa75ed3037c1b7ced1c74f5266beb0008
Chris Sanders <NinjaZidane@hotmail.com>
2012-07-19 02:51:52 -0400

    [Bukkit] Debug Signed-off-by: Chris Sanders <NinjaZidane@hotmail.com>

---
+++ src/main/java/com/almuramc/backpack/spout/input/BackpackInput.java
@@ -0,0 +1,10 @@
+package com.almuramc.backpack.spout.input;
+
+import org.spout.api.keyboard.Input;
+
+public class BackpackInput implements Input {
+	@Override
+	public void bind(String key, String command) {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+}
