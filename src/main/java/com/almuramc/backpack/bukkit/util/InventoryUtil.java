4062f3f3a019119c98f43fb035ea0789860916c1
Chris Sanders <zidane@outlook.com>
2012-09-14 00:59:27 -0400

    Cleanup.
    Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/com/almuramc/backpack/bukkit/util/InventoryUtil.java
+++ src/main/java/com/almuramc/backpack/bukkit/util/InventoryUtil.java
@@ -1,16 +1,35 @@
+/*
+ * This file is part of Backpack.
+ *
+ * Copyright (c) 2012, AlmuraDev <http://www.almuramc.com/>
+ * Backpack is licensed under the Almura Development License version 1.
+ *
+ * Backpack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * As an exception, all classes which do not reference GPL licensed code
+ * are hereby licensed under the GNU Lesser Public License, as described
+ * in Almura Development License version 1.
+ *
+ * Backpack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License,
+ * the GNU Lesser Public License (for classes that fulfill the exception)
+ * and the Almura Development License version 1 along with this program. If not, see
+ * <http://www.gnu.org/licenses/> for the GNU General Public License and
+ * the GNU Lesser Public License.
+ */
 package com.almuramc.backpack.bukkit.util;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Chris
- * Date: 9/14/12
- * Time: 12:53 AM
- * To change this template use File | Settings | File Templates.
- */
 public class InventoryUtil {
 	public static boolean hasOnlyOneFreeSlot(Inventory inv) {
 		if (inv == null) {
